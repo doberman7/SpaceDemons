9f52171dd3a26f18b0540f79b747613606952d95
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-24 18:16:07 -0500

    added clases motherShip and armis

--- classes.js
+++ classes.js
@@ -18,6 +18,85 @@ class Board{
   }
 
 }
+// Factory oof ships
+class motherShip {
+  constructor(health, strength,xAxis, yAxis∫) {
+    this.health = health,
+    this.strength = strength,
+    this.xAxis = xAxis,
+    this.yAxis = yAxis
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage
+    return
+  }
+}
 
-// let board = new Board()
-  // board.draw()
+
+// nave 1
+class Nave1 extends motherShip {
+  constructor(type, health, strength) {
+    super(type, health, strength)
+    this.type = type,
+      this.health = health,
+      this.strength = strength
+  }
+  // should return "NAME has received DAMAGE points of damage", if the Nave1 is still alive
+  receiveDamage(damage) {
+    this.health = this.health - damage
+
+    if (this.health > 0) {
+      this.alive = true;
+      // return `${this.type} has received ${damage} points of damage`
+    } else if (this.health === 0) {
+      this.alive = false;
+      // return `${this.type} has died in act of combat`
+
+    }
+  }
+
+  // battleCry() {
+  //   return `Odin Owns You All!`
+  // }
+
+}
+
+// nave2
+class nave2 extends motherShip {
+  receiveDamage(damage) {
+    this.health = this.health - damage
+
+    if (this.health > 0) {
+      this.alive = true;
+      // return `A nave2 has received ${damage} points of damage`
+    } else if (this.health === 0) {
+      this.alive = false;
+      // return `A nave2 has died in combat`
+
+    }
+  }
+
+
+}
+
+// War
+class combat {
+  constructor(){
+    this.nave1Army=[]
+    this.nave2Army=[]
+  }
+  addNave1(nave1){
+    this.nave1Army.push(nave1)
+  }
+  addnave2(nave2){
+    this.nave2Army.push(nave2)
+  }
+  nave1Attack(){
+    nave2.receiveDamage=Nave1.strength
+  }
+  nave2Attack(){}
+  showStatus(){}
+}
