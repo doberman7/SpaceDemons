d44e2b55ca07a8409abf6970e0f3662dd6ef886e
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-24 17:11:31 -0500

    draw space

---
+++ classes.js
@@ -0,0 +1,23 @@
+class Board{
+  constructor(){
+    this.x = 0
+    this.y = 0
+    this.width = $canvas.width
+    this.height = $canvas.height
+    this.img = new Image()
+    this.img.src = './images/fondo.jpg'
+    this.img.onload = () => {
+      this.draw()
+    }
+  }
+  draw(){
+    // if(this.x <-$canvas.width) this.x = 0      //esto fué para mover a la izquierda
+    // this.x--
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    // ctx.drawImage(this.img, this.x + $canvas.width, this.y, this.width, this.height)
+  }
+
+}
+
+let board = new Board()
+  // board.draw()
--- key.js
+++
@@ -1,7 +0,0 @@
-
-// Esto es una prueba
-// Esto es otra prueba
-
-sopa de pollo
-
-nuevos cambios de lo que hice yo por aca
